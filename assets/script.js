e0c9852e73c55191ae10f9a9ba4b4ad661f77fd0
Alyshia Kandler <a.lyshia@hotmail.com>
2023-01-24 17:15:38 +1100

    previous search buttons and local storage added

--- assets/script.js
+++ assets/script.js
@@ -3,11 +3,14 @@ var obj = { arrayOfPreviousSearches: [] };
 
 document.getElementById('search-button').addEventListener('click', clearPreviousSearch)
 
-function clearPreviousSearch(){
+function clearPreviousSearch() {
+
+    
     document.getElementById('current-city').textContent = ""
     document.getElementById('temperature').textContent = ""
     document.getElementById('humidity').textContent = ""
     document.getElementById('wind-speed').textContent = ""
+    document.getElementById('currentImg').textContent = ""
 
     //day 1
     document.getElementById('futureDate0').textContent = ""
@@ -37,80 +40,117 @@ function clearPreviousSearch(){
     fetchAPI()
 }
 
-function fetchAPI(){
-console.log('FetchAPI working')
-let citySearch = document.getElementById('city-search').value;
-if (citySearch === ''){
-document.getElementById('error-message').textContent = "Please enter a city"
-return }
-let latLonUrl = 'http://api.openweathermap.org/geo/1.0/direct?q='+ citySearch +'&limit=5&appid=' + apiKey;
-// make a call to that new api
-fetch(latLonUrl)
-    .then(function(resp) {
-        console.log(resp);
-        return resp.json();
-    })
-    .then(function(geocodeData) {
-        // do whatwer w/ that
-        console.log(geocodeData)
-        // extract the data you need to place you next order
-        var latitude = geocodeData[0].lat
-        var longitude = geocodeData[0].lon
-        // put that data into the url you need for the next oder
-        var fiveDayURL = 'https://api.openweathermap.org/data/2.5/forecast?lat=' + latitude + '&lon=' + longitude + '&appid=' + apiKey;
-        // place the next order (fetch)
-        // fetch
-        fetch(fiveDayURL).then(function(response){
-            return response.json();
-
-        }).then(function(fiveDayData){
-            console.log(fiveDayData)
-            var fiveDayArray = fiveDayData.list
-            console.log(fiveDayArray)
-            //current
-            let img = document.createElement("img"); 
-            img.src = "http://openweathermap.org/img/wn/" + fiveDayArray[0].weather[0].icon + "@2x.png";
-            document.getElementById('currentImg').appendChild(img);
-            document.getElementById('current-city').append(geocodeData[0].local_names.en)
-            document.getElementById('temperature').append(' ' + (fiveDayArray[0].main.temp-273.15).toFixed(2) + '°C')
-            document.getElementById('humidity').append(' ' + fiveDayArray[0].main.humidity + '%')
-            document.getElementById('wind-speed').append(' ' + fiveDayArray[0].wind.speed + ' MPH')
-
-            //day 1
-            let img0 = document.createElement("img"); 
-            img0.src = "http://openweathermap.org/img/wn/" + fiveDayArray[7].weather[0].icon + "@2x.png";
-            document.getElementById('futureImg0').appendChild(img0);
-            document.getElementById('futureDate0').append(fiveDayArray[7].dt_txt)
-            document.getElementById('futureTemp0').append(' ' + (fiveDayArray[7].main.temp-273.15).toFixed(2) + '°C')
-            document.getElementById('futureHumidity0').append(' ' + fiveDayArray[7].main.humidity + '%')
-            //day2
-            let img1 = document.createElement("img"); 
-            img1.src = "http://openweathermap.org/img/wn/" + fiveDayArray[15].weather[0].icon + "@2x.png";
-            document.getElementById('futureImg1').appendChild(img1);
-            document.getElementById('futureDate1').append(fiveDayArray[15].dt_txt)
-            document.getElementById('futureTemp1').append(' ' + (fiveDayArray[15].main.temp-273.15).toFixed(2) + '°C')
-            document.getElementById('futureHumidity1').append(' ' + fiveDayArray[15].main.humidity + '%')
-            //day3
-            let img2 = document.createElement("img"); 
-            img2.src = "http://openweathermap.org/img/wn/" + fiveDayArray[23].weather[0].icon + "@2x.png";
-            document.getElementById('futureImg2').appendChild(img2);
-            document.getElementById('futureDate2').append(fiveDayArray[23].dt_txt)
-            document.getElementById('futureTemp2').append(' ' + (fiveDayArray[23].main.temp-273.15).toFixed(2) + '°C')
-            document.getElementById('futureHumidity2').append(' ' + fiveDayArray[23].main.humidity + '%')
-            //day4
-            let img3 = document.createElement("img"); 
-            img3.src = "http://openweathermap.org/img/wn/" + fiveDayArray[31].weather[0].icon + "@2x.png";
-            document.getElementById('futureImg3').appendChild(img3);
-            document.getElementById('futureDate3').append(fiveDayArray[31].dt_txt)
-            document.getElementById('futureTemp3').append(' ' + (fiveDayArray[31].main.temp-273.15).toFixed(2) + '°C')
-            document.getElementById('futureHumidity3').append(' ' + fiveDayArray[31].main.humidity + '%')
-            //day 5
-            let img4 = document.createElement("img"); 
-            img4.src = "http://openweathermap.org/img/wn/" + fiveDayArray[39].weather[0].icon + "@2x.png";
-            document.getElementById('futureImg4').appendChild(img4);
-            document.getElementById('futureDate4').append(fiveDayArray[39].dt_txt)
-            document.getElementById('futureTemp4').append(' ' + (fiveDayArray[39].main.temp-273.15).toFixed(2) + '°C')
-            document.getElementById('futureHumidity4').append(' ' + fiveDayArray[39].main.humidity + '%')
+function fetchAPI() {
+    console.log('FetchAPI working')
+    let citySearch = document.getElementById('city-search').value;
+    if (citySearch === '') {
+        document.getElementById('error-message').textContent = "Please enter a city"
+        return
+    }
+    document.getElementById('city-search').value = "";
+    let latLonUrl = 'http://api.openweathermap.org/geo/1.0/direct?q=' + citySearch + '&limit=5&appid=' + apiKey;
+    // make a call to that new api
+    fetch(latLonUrl)
+        .then(function (resp) {
+            console.log(resp);
+            return resp.json();
         })
-    })
+        .then(function (geocodeData) {
+
+            // if i got here then the city i typed is most likely the city i wanted
+          
+            // take the city that was searched
+            const button = document.createElement('button')
+            $(button).addClass('btn btn-primary m-2');
+            button.innerHTML = citySearch
+            if(document.getElementById('city-search').value === $(button).value){
+                return
+            }
+            else{
+            // put in on the screen in the designated area
+           document.querySelector('#previous-searches').appendChild(button)
+            // when button is clicked value is searched
+            button.addEventListener('click', function(){
+                // get the city that was clicked (innerhtml of the button)
+                var theCity = citySearch;
+                // reach into the html and get the input box
+                var inputBox = document.querySelector('#city-search')
+                // put it in the search boxs value
+                inputBox.value = theCity
+                // call the fetchAPI function
+                clearPreviousSearch();
+            })
+            }
+            let cities = citySearch
+            let citiesJSON = JSON.stringify(cities)
+            localStorage.setItem('searchedCities', citiesJSON);
+            let citiesFromLS = localStorage.getItem('searchedCities');
+            let citiesAsJS = JSON.parse(citiesJSON);
+            let newCity = cities;
+            //citiesAsJS.push(newCity);
+            console.log(citiesAsJS);
+            button.innerHTML = citiesAsJS
+            // do whatwer w/ that
+            console.log(geocodeData)
+            // extract the data you need to place you next order
+            var latitude = geocodeData[0].lat
+            var longitude = geocodeData[0].lon
+            // put that data into the url you need for the next oder
+            var fiveDayURL = 'https://api.openweathermap.org/data/2.5/forecast?lat=' + latitude + '&lon=' + longitude + '&appid=' + apiKey;
+            // place the next order (fetch)
+            // fetch
+            fetch(fiveDayURL).then(function (response) {
+                return response.json();
+            }).then(function (fiveDayData) {
+                console.log(fiveDayData)
+                var fiveDayArray = fiveDayData.list
+                console.log(fiveDayArray)
+                //current
+                let img = document.createElement("img");
+                img.src = "http://openweathermap.org/img/wn/" + fiveDayArray[0].weather[0].icon + "@2x.png";
+                document.getElementById('currentImg').appendChild(img);
+                document.getElementById('current-city').append(geocodeData[0].local_names.en)
+                document.getElementById('temperature').append(' ' + (fiveDayArray[0].main.temp - 273.15).toFixed(2) + '°C')
+                document.getElementById('humidity').append(' ' + fiveDayArray[0].main.humidity + '%')
+                document.getElementById('wind-speed').append(' ' + fiveDayArray[0].wind.speed + ' MPH')
+
+                //day 1
+                let img0 = document.createElement("img");
+                img0.src = "http://openweathermap.org/img/wn/" + fiveDayArray[7].weather[0].icon + "@2x.png";
+                document.getElementById('futureImg0').appendChild(img0);
+                document.getElementById('futureDate0').append(fiveDayArray[7].dt_txt)
+                document.getElementById('futureTemp0').append(' ' + (fiveDayArray[7].main.temp - 273.15).toFixed(2) + '°C')
+                document.getElementById('futureHumidity0').append(' ' + fiveDayArray[7].main.humidity + '%')
+                //day2
+                let img1 = document.createElement("img");
+                img1.src = "http://openweathermap.org/img/wn/" + fiveDayArray[15].weather[0].icon + "@2x.png";
+                document.getElementById('futureImg1').appendChild(img1);
+                document.getElementById('futureDate1').append(fiveDayArray[15].dt_txt)
+                document.getElementById('futureTemp1').append(' ' + (fiveDayArray[15].main.temp - 273.15).toFixed(2) + '°C')
+                document.getElementById('futureHumidity1').append(' ' + fiveDayArray[15].main.humidity + '%')
+                //day3
+                let img2 = document.createElement("img");
+                img2.src = "http://openweathermap.org/img/wn/" + fiveDayArray[23].weather[0].icon + "@2x.png";
+                document.getElementById('futureImg2').appendChild(img2);
+                document.getElementById('futureDate2').append(fiveDayArray[23].dt_txt)
+                document.getElementById('futureTemp2').append(' ' + (fiveDayArray[23].main.temp - 273.15).toFixed(2) + '°C')
+                document.getElementById('futureHumidity2').append(' ' + fiveDayArray[23].main.humidity + '%')
+                //day4
+                let img3 = document.createElement("img");
+                img3.src = "http://openweathermap.org/img/wn/" + fiveDayArray[31].weather[0].icon + "@2x.png";
+                document.getElementById('futureImg3').appendChild(img3);
+                document.getElementById('futureDate3').append(fiveDayArray[31].dt_txt)
+                document.getElementById('futureTemp3').append(' ' + (fiveDayArray[31].main.temp - 273.15).toFixed(2) + '°C')
+                document.getElementById('futureHumidity3').append(' ' + fiveDayArray[31].main.humidity + '%')
+                //day 5
+                let img4 = document.createElement("img");
+                img4.src = "http://openweathermap.org/img/wn/" + fiveDayArray[39].weather[0].icon + "@2x.png";
+                document.getElementById('futureImg4').appendChild(img4);
+                document.getElementById('futureDate4').append(fiveDayArray[39].dt_txt)
+                document.getElementById('futureTemp4').append(' ' + (fiveDayArray[39].main.temp - 273.15).toFixed(2) + '°C')
+                document.getElementById('futureHumidity4').append(' ' + fiveDayArray[39].main.humidity + '%')
+            })
+        })
 }
+
+//when search is clicked, if fetch API is successful 
